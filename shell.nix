e2c2f85e8754fb8a5cb4ec1c850b94f2fdcfa7f2
Niklas Hambüchen <mail@nh2.me>
2025-02-14 05:03:48 +0100

    shell.nix: Remove no longer necessary overrides

--- shell.nix
+++ shell.nix
@@ -12,14 +12,6 @@ in
   myEnv = myStdenv.mkDerivation {
     name = "my-env";
     buildInputs = [
-      (protobuf.override (old: {
-        stdenv = myStdenv;
-        # Need to also override `protobuf`'s dependencies with the older GCC,
-        # otherwise building `protobuf` itself gives linker errors.
-        abseil-cpp = pkgs.abseil-cpp.override { stdenv = myStdenv; };
-        gtest = pkgs.gtest.override { stdenv = myStdenv; };
-      }))
-      pkg-config
     ];
   };
 }
